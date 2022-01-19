e5cb76b3ad3a13a3a27fdf3f4c7a75428b4986a6
Hoang Long <longnguyen20011123@gmail.com>
2022-01-19 23:57:37 +0700

    bai tap 1

---
+++ Week2/lab01_9033_OOP/Baitap1/Tester.cs
@@ -0,0 +1,35 @@
+﻿namespace Baitap1
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter the number of students: ");
+            int n = int.Parse(System.Console.ReadLine());
+
+            Student[] students = new Student[n];
+
+            System.Console.WriteLine("Input student information");
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Id: ");
+                int id = int.Parse(System.Console.ReadLine());
+                System.Console.Write("Name: ");
+                string name = System.Console.ReadLine();
+                System.Console.Write("Faculty: ");
+                string faculty = System.Console.ReadLine();
+                System.Console.Write("Score: ");
+                float score = float.Parse(System.Console.ReadLine());
+
+                students[i] = new Student(id, name, faculty, score);
+            }
+            System.Console.WriteLine("----------------------------------");
+            System.Console.WriteLine("Output student information");
+            foreach (var item in students)
+            {
+                item.Show();
+            }
+
+        }
+    }
+}
